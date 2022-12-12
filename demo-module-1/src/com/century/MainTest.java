a5fb1aad7b7afeacda6f43f7e7d549922cf0935c
lq0023 <503855429@qq.com>
2022-12-12 15:11:38 +0800

    尝试进行第五次更改-测试回退

--- demo-module-1/src/com/century/MainTest.java
+++ demo-module-1/src/com/century/MainTest.java
@@ -3,5 +3,6 @@ package com.century;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git 回退操作！");  //此次新增了这行打印语句
     }
 }
