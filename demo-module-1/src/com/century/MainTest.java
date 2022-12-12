82e87209b0fe104bcf4fea43f6f95d79dd8a01fe
lq0023 <503855429@qq.com>
2022-12-12 16:24:18 +0800

    分支冲突测试-test中增加打印hellO test

--- demo-module-1/src/com/century/MainTest.java
+++ demo-module-1/src/com/century/MainTest.java
@@ -3,5 +3,6 @@ package com.century;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello test");
     }
 }
